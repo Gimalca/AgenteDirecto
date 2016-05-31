9fc71b69c8aca3cb86b00db2e1975206fa2fca2f
David Lares <david.e.lares@gmail.com>
2016-05-31 14:53:52 -0400

    Functional Fixes

--- assets/js/configs/config-tour.js
+++ assets/js/configs/config-tour.js
@@ -10,19 +10,28 @@
     steps: [{
       element: "#toggleMenubar",
       position: "right",
-      intro: "Offcanvas Menu <p class='content'>It is nice custom navigation for desktop users and a seek off-canvas menu for tablet and mobile users</p>"
+      intro: "Bienvenidos a AgenteDirecto <p class='content'>Aquí podemos poner información para explicar este paso</p>"
     }, {
-      element: "#toggleFullscreen",
-      intro: "Full Screen <p class='content'>Click this button you can view the admin template in full screen</p>"
+      element: "#step1",
+      intro: "Busca lo que necesitas <p class='content'>Aquí podemos poner información para explicar este paso</p>"
     }, {
-      element: "#toggleChat",
+      element: "#step2",
       position: 'left',
-      intro: "Quick Conversations <p class='content'>This is a sidebar dialog box for user conversations list, you can even create a quick conversation with other users</p>"
-    }],
+      intro: "Elige el rubro o categoría que necesites <p class='content'>Aquí podemos poner información para explicar este paso</p>"
+    }, {
+      element: "#step3",
+      position: 'left',
+      intro: "País o locación de tu preferencia <p class='content'>Aquí podemos poner información para explicar este paso</p>"
+    }, {
+      element: "#step4",
+      position: 'left',
+      intro: "A un solo click <p class='content'>Miles de oportunidades en un solo lugar</p>"
+    }
+    ],
     skipLabel: "<i class='md-close'></i>",
     doneLabel: "<i class='md-close'></i>",
-    nextLabel: "Next <i class='md-chevron-right'></i>",
-    prevLabel: "<i class='md-chevron-left'></i>Prev",
+    nextLabel: "Siguiente <i class='md-chevron-right'></i>",
+    prevLabel: "<i class='md-chevron-left'></i>Atrás",
     showBullets: false
   });
 
